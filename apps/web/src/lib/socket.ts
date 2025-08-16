78ab00e7fad5ef7f67cfc4c6c2afb4b071605af3
fsseer <fsseer@naver.com>
2025-08-17 00:29:41 +0900

    fix(web): use ESM import for socket.io-msgpack-parser on client

--- apps/web/src/lib/socket.ts
+++ apps/web/src/lib/socket.ts
@@ -1,7 +1,5 @@
 import { io } from 'socket.io-client'
-// MessagePack parser client side
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { MsgPackParser } = require('socket.io-msgpack-parser')
+import msgpackParser from 'socket.io-msgpack-parser'
 
 // Vite 환경에서만 존재하는 import.meta.env 접근을 안전하게 처리
 type ViteMetaEnv = { VITE_SERVER_ORIGIN?: string }
@@ -42,5 +40,5 @@ export const socket = io(envOrigin && envOrigin.length > 0 ? envOrigin : default
   upgrade: !isTunnel,
   path: '/socket.io',
   timeout: 10000,
-  parser: MsgPackParser,
+  parser: msgpackParser,
 })
